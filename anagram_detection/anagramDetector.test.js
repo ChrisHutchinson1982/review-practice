15ad534289b313a3c3d442204f2d07a3a19e88f4
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-01-05 21:56:20 +0000

    passed green on 5th test

--- anagram_detection/anagramDetector.test.js
+++ anagram_detection/anagramDetector.test.js
@@ -17,4 +17,8 @@ describe("anagramDetector", () => {
     const anagramDetector = new AnagramDetector();
     expect(anagramDetector.isAnagram("foefet", "toffee")).toBe(true);
   });
+  it("return true when 2nd is anagram of 1st and case difference", () => {
+    const anagramDetector = new AnagramDetector();
+    expect(anagramDetector.isAnagram("Buckethead", "DeathCubeK")).toBe(true);
+  });
 });
