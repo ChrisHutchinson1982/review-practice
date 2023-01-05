e3ab9ac89580fda29eaee78be885a61d809d3f7f
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-01-05 21:20:10 +0000

    Refactored

--- string_ends_with/stringChecker.js
+++ string_ends_with/stringChecker.js
@@ -1,11 +1,6 @@
 class StringChecker {
   solution(str, ending) {
-    const endingLength = ending.length;
-    if (endingLength === 0) {
-      return true;
-    } else {
-      return str.slice(-endingLength) === ending;
-    }
+    return str.endsWith(ending);
   }
 }
 
