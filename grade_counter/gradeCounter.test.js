9a865e0258ebcaabb5ef1223d54b4328df6f6eda
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-01-06 10:27:13 +0000

    passed next two tests

--- grade_counter/gradeCounter.test.js
+++ grade_counter/gradeCounter.test.js
@@ -19,8 +19,20 @@ describe("count grades and return string", () => {
     const gradeCounter = new GradeCounter();
     expect(gradeCounter.countGrades("Red, Red")).toBe("Red: 2\n");
   });
-  it("when mutiple instances of Amaber", () => {
+  it("when mutiple instances of Amber", () => {
     const gradeCounter = new GradeCounter();
     expect(gradeCounter.countGrades("Amber, Amber")).toBe("Amber: 2\n");
+  });
+  it("when mutiple instances of Green, Red and Amber", () => {
+    const gradeCounter = new GradeCounter();
+    expect(
+      gradeCounter.countGrades("Green, Red, Green, Red, Red, Amber, Amber")
+    ).toBe("Green: 2\nRed: 3\nAmber: 2\n");
+  });
+  it("when mutiple instances of Red and Amber", () => {
+    const gradeCounter = new GradeCounter();
+    expect(gradeCounter.countGrades("Red, Red, Amber, Amber")).toBe(
+      "Red: 2\nAmber: 2\n"
+    );
   });
 });
