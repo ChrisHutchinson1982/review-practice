29bb9264552baf998b6ab6b53b9012c84ae1c405
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-01-06 10:20:30 +0000

    passed third test

--- grade_counter/gradeCounter.js
+++ grade_counter/gradeCounter.js
@@ -2,15 +2,30 @@ class GradeCounter {
   countGrades(string) {
     const arrayOfGrades = string.split(", ");
     let greenCount = 0;
+    let redCount = 0;
 
     arrayOfGrades.forEach((grade) => {
       if (grade === "Green") {
         greenCount += 1;
+      } else if (grade === "Red") {
+        redCount += 1;
       }
     });
 
-    console.log(`${greenCount}`);
-    return `Green: ${greenCount}\n`;
+    let greenString = "";
+    let redString = "";
+
+    if (greenCount > 0) {
+      greenString = `Green: ${greenCount}\n`;
+    }
+
+    if (redCount > 0) {
+      redString = `Red: ${redCount}\n`;
+    }
+
+    console.log(redString);
+
+    return `${greenString}${redString}`;
   }
 }
 
--- grade_counter/gradeCounter.test.js
+++ grade_counter/gradeCounter.test.js
@@ -12,7 +12,11 @@ describe("count grades and return string", () => {
   it("when mutiple instances of Green and Red", () => {
     const gradeCounter = new GradeCounter();
     expect(gradeCounter.countGrades("Green, Green, Green, Red, Red")).toBe(
-      "Green: 3\n Red2\n"
+      "Green: 3\nRed: 2\n"
     );
+  });
+  it("when mutiple instances of Red", () => {
+    const gradeCounter = new GradeCounter();
+    expect(gradeCounter.countGrades("Red, Red")).toBe("Red: 2\n");
   });
 });
