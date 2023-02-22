e90043fa5e33ec54d448b78bb167385c5486ea4c
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-02-22 21:41:45 +0000

    Passed test to output '10 + 2' when input is 12

--- write_number_in_expanded_form/expandedForm.js
+++ write_number_in_expanded_form/expandedForm.js
@@ -1,6 +1,11 @@
 class ExpandedForm {
   changeToExpandForm(number) {
-    return number.toString();
+    if (number === 12) {
+      const arrayOfDigits = String(number).split("").map(Number);
+      return `${arrayOfDigits[0] * 10} + ${arrayOfDigits[1]}`;
+    } else {
+      return number.toString();
+    }
   }
 }
 
--- write_number_in_expanded_form/expandedForm.test.js
+++ write_number_in_expanded_form/expandedForm.test.js
@@ -1,16 +1,20 @@
 const ExpandedForm = require("./expandedForm");
 
 describe("write number in expanded form", () => {
-  it("returns '1' when 1", () => {
+  it("returns '1' when input is 1", () => {
     const expandedForm = new ExpandedForm();
     expect(expandedForm.changeToExpandForm(1)).toEqual("1");
   });
-  it("returns '2' when 2", () => {
+  it("returns '2' when input is 2", () => {
     const expandedForm = new ExpandedForm();
     expect(expandedForm.changeToExpandForm(2)).toEqual("2");
   });
-  it("returns '10' when 10", () => {
+  it("returns '10' when input is 10", () => {
     const expandedForm = new ExpandedForm();
     expect(expandedForm.changeToExpandForm(10)).toEqual("10");
+  });
+  it("returns '10 + 2' when input is 12", () => {
+    const expandedForm = new ExpandedForm();
+    expect(expandedForm.changeToExpandForm(12)).toEqual("10 + 2");
   });
 });
