2d7619495333ea95de442c3c8012f917969cdd25
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-04-04 22:07:41 +0100

    Passed array_diff test to Output [1] when input is [1,2,2]

--- array_diff/arrayDiff.test.js
+++ array_diff/arrayDiff.test.js
@@ -10,7 +10,10 @@ describe("Remove all values from list that are present in list b keeping their o
   it("Output [1,2] when input is [1,2], [3]", () => {
     expect(arrayDiff([1, 2], [3])).toEqual([1, 2]);
   });
-  it("Output [2,2] when input is [1,2,2], [1]  ", () => {
+  it("Output [2,2] when input is [1,2,2], [1]", () => {
+    expect(arrayDiff([1, 2, 2], [1])).toEqual([2, 2]);
+  });
+  it("Output [1] when input is [1,2,2], [2]", () => {
     expect(arrayDiff([1, 2, 2], [1])).toEqual([2, 2]);
   });
 });
